bcbb1e21756011d002d09acf0bd27b971659fba6
Александр Юнцевич <alexunc@hotmail.com>
2015-12-19 21:27:19 +0500

    Тестовый комит.

--- ShtatRaspisanie/Form1.Designer.cs
+++ ShtatRaspisanie/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(31, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 40);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(150, 65);
+            this.ClientSize = new System.Drawing.Size(192, 66);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
