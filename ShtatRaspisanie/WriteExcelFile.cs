26699ba5b5bf475517e96afaa31753106c78f512
Alex <alexunc@mail.ru>
2015-12-21 01:27:02 +0500

    Промежуточный коммит.

--- ShtatRaspisanie/WriteExcelFile.cs
+++ ShtatRaspisanie/WriteExcelFile.cs
@@ -5,28 +5,64 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using OfficeOpenXml;
 
 namespace ShtatRaspisanie
 {
     class WriteExcelFile
     {
-        public static void writeShtatnoeRaspisanie(ArrayList podrazdelenieList, ArrayList shtatnEdinicaList)
-        {
-            if (podrazdelenieList != null && shtatnEdinicaList != null)
-            {
-                Console.WriteLine("WriteExcelFile1");
-            }
-        }
+
 
         internal static void writeShtatnoeRaspisanie(Action<FileStream> parseSpisokPodrazdeleniyFile, Action<FileStream> parseSpisokShtatnEdinicaFile)
         {
 
             if (ParseExcelFile.isSpisokShtatnEdinicaFileExist && ParseExcelFile.isSpisokPodrazdeleniyFileExist)
             {
-                Console.WriteLine("WriteExcelFile2");
+                string fileName = @"d:\ShtatnoeRaspisanie.xlsx";
+                // Create a spreadsheet document by supplying the filepath.
+                // By default, AutoSave = true, Editable = true, and Type = xlsx.
+                SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.
+                    Create(fileName, SpreadsheetDocumentType.Workbook);
+
+                // Add a WorkbookPart to the document.
+                WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+                workbookpart.Workbook = new Workbook();
+
+                // Add a WorksheetPart to the WorkbookPart.
+                WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+                // Add Sheets to the Workbook.
+                Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                    AppendChild<Sheets>(new Sheets());
+
+                // Append a new worksheet and associate it with the workbook.
+                Sheet sheet = new Sheet()
+                {
+                    Id = spreadsheetDocument.WorkbookPart.
+                    GetIdOfPart(worksheetPart),
+                    SheetId = 1,
+                    Name = "Штатное расписание"
+                    
+                };
+                SheetData sheetData = new SheetData();
+ 
+                sheets.Append(sheet);
+
+                workbookpart.Workbook.Save();
+
+                // Close the document.
+                spreadsheetDocument.Close();
+
             } else
             {
-                Console.WriteLine("Выберите оба файла.");
+              
+                MessageBox.Show("Выберите оба файла.");
+                
             }
         }
     }
