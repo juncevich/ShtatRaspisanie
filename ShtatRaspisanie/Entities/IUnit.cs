b47d7a124e80f3633f72a87cddddb053235c3257
Alex <a.juncevich@gmail.com>
2016-01-16 14:13:11 +0500

    Add interface IUnit.

---
+++ ShtatRaspisanie/Entities/IUnit.cs
@@ -0,0 +1,10 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+
+namespace ShtatRaspisanie.Entities
+{
+    public interface IUnit
+    {
+        void InitUnit();
+        void DisplayUnit();
+    }
+}
